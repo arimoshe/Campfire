c58f2cb73a48e05344fcc4102b26ef4cfd0e9e55
Ari Moshe <arimoshe@gmail.com>
2022-09-04 23:13:31 -0700

    Add session and users controller and jbuilder views

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,10 +1,31 @@
 class Api::SessionsController < ApplicationController
+
+  before_action :require_logged_in, only: [:destroy, :show]
+
   def show
+      if @user = current_user
+          render 'api/users/show'
+      else
+          render json: { user: nil }
+      end
   end
 
   def create
+      #expect params to have username and password
+      email = params[:email]
+      password = params[:password]
+      @user = User.find_by_credentials(email, password)
+      if @user
+          login!(@user)
+          render   'api/users/show'
+      else
+          render json: { errors: ['Invalid credentials']}, status: 422
+      end
   end
 
-  def destroy
+  def destroy 
+      logout! if logged_in?
+      render json: { message: 'success' }
+      #response will have no body
   end
 end
--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,4 +1,22 @@
 class Api::UsersController < ApplicationController
+  
+  wrap_parameters include: User.attribute_names + ['password']
+
   def create
+    @user = User.new(user_params)
+    if @user.save
+        login!(@user)
+        render :show
+    else
+        render json: @user.errors.full_messages, status: 422 #:unprossessable_entity  
+    end
   end
+
+    private
+
+    def user_params
+      params.require(:user).permit(:email, :username, :password)
+    end
+
+
 end
